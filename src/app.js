125de670593c99e7e0d5ed5c83cddeae5f8356da
Shorya Rastogi <shoryarastogi99@gmail.com>
2020-06-01 21:10:28 +0530

    Remove Dummy Expnese Data

--- src/app.js
+++ src/app.js
@@ -8,22 +8,17 @@ import { setTextFilter, sortByAmount } from './actions/filters';
 import getVisibleExpense from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 
 const store = configureStore();
-const expenseOne = store.dispatch(addExpense({ description: 'Water Bill', amount: 250, createdAt: 3000 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Electricity Bill', amount: 3000, createdAt: 1000 }));
-const expenseThree = store.dispatch(addExpense({ description: 'Gas Bill', amount: 500, createdAt: 1500 }));
+// const expenseOne = store.dispatch(addExpense({ description: 'Water Bill', amount: 250, createdAt: 3000 }));
+// const expenseTwo = store.dispatch(addExpense({ description: 'Electricity Bill', amount: 3000, createdAt: 1000 }));
+// const expenseThree = store.dispatch(addExpense({ description: 'Gas Bill', amount: 500, createdAt: 1500 }));
 
-// store.dispatch(setTextFilter('bill'));
-
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('water'))
-// }, 3000);
-
-const state = store.getState();
-const visibleExpense = getVisibleExpense(state.expenses, state.filters);
-console.log(visibleExpense); 
+// const state = store.getState();
+// const visibleExpense = getVisibleExpense(state.expenses, state.filters);
+// console.log(visibleExpense); 
 
 const jsx =(
     <Provider store ={store}>
